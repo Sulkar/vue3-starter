c3f8ee748e3f0ce32a4e4a50b742c3362f056ed1
Richard Scheglmann <r.scheglmann@gmail.com>
2022-11-27 22:23:26 +0100

    init

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# vue3-starter
+# vue3-starter - options API
+
+This template should help get you started developing with Vue 3 in Vite.
+
+![](/data/info.png)
+
+## Customize configuration
+
+See [Vite Configuration Reference](https://vitejs.dev/config/).
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Compile and Minify for Production
+
+```sh
+npm run build
+```
---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<template>
+
+  <main>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  
+}
+</style>
